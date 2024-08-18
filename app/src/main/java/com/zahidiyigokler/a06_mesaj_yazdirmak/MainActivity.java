e005fe905a6e69da83e65130e364822736cea159
Zahid İyigökler <iyigoklerzahid55@gmail.com>
2024-08-18 13:24:49 +0300

    The file has been simplified.

--- app/src/main/java/com/zahidiyigokler/a06_mesaj_yazdirmak/MainActivity.java
+++ app/src/main/java/com/zahidiyigokler/a06_mesaj_yazdirmak/MainActivity.java
@@ -4,27 +4,16 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
         // Context
         // Activity Context
 
@@ -33,13 +22,9 @@ public class MainActivity extends AppCompatActivity {
         //getApplicationContext()
 
         Toast.makeText(this,"Toast Message",Toast.LENGTH_LONG).show();
-
     }
-
     public void save(View view) {
-
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-
         alert.setTitle("Save");
         alert.setMessage("Are you sure?");
         alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -56,7 +41,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         alert.show();
-
     }
-
 }
